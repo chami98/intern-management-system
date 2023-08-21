66b9365075f4130eabf66b4838f6190a6253ba7e
chami98 <chamikara98@gmail.com>
2023-08-21 11:09:31 +0530

    Clear the state when closing the modal

--- src/components/createUserAccountDialog.js
+++ src/components/createUserAccountDialog.js
@@ -17,6 +17,8 @@ import {
   InputLabel,
   FormControl,
   Autocomplete,
+  Backdrop,
+  CircularProgress,
 } from "@mui/material";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
@@ -33,7 +35,7 @@ export default function CreateUserAccountDialog({
   title,
   open,
   handleClickOpen,
-  handleClose,
+  handleClose: parentHandleClose,
 }) {
   const [showPassword, setShowPassword] = React.useState(false);
   const [formData, setFormData] = React.useState({
@@ -46,6 +48,7 @@ export default function CreateUserAccountDialog({
   const [role, setRole] = useState("");
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState(false);
+  const [loading, setLoading] = React.useState(false);
 
   // Regular expression for basic email validation
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -100,6 +103,7 @@ export default function CreateUserAccountDialog({
   };
 
   const handleSave = () => {
+    setLoading(true);
     axios
       .post("http://localhost:5000/api/register", formData)
       .then((response) => {
@@ -117,12 +121,13 @@ export default function CreateUserAccountDialog({
           progress: undefined,
         });
 
+        setLoading(false);
+
         // Close the dialog after saving
 
         setTimeout(() => {
           handleClose();
         }, 1500);
-        
       })
       .catch((error) => {
         if (error.response.status === 409) {
@@ -148,9 +153,26 @@ export default function CreateUserAccountDialog({
         }
 
         console.error("Error occurred during the request:", error);
+        setLoading(false);
       });
 
     console.log("Form data to be sent:", formData);
+  };
+
+  const handleClose = () => {
+    setFormData({
+      first_name: "",
+      last_name: "",
+      email: "",
+      role: null,
+      password: "",
+    });
+    setEmailError("");
+    setPasswordError("");
+    setRole("");
+    setShowPassword(false);
+    
+    parentHandleClose();
   };
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
@@ -291,6 +313,13 @@ export default function CreateUserAccountDialog({
           </Grid>
         </Box>
       </Dialog>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loading}
+        onClick={handleClose}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </div>
   );
 }
